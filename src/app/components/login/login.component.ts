b18d493f3df06ef3cf8a5f4393c85a0c4481625d
Abdulrahman Al-Ruwaishan <sanyemaa@gmail.com>
2019-07-17 14:21:35 -0400

    add authentication, login form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { FlashMessagesService } from 'angular2-flash-messages';
+
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  email: string;
+  password: string;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private flashMessage: FlashMessagesService
+  ) { }
 
   ngOnInit() {
+    this.authService.getAuth().subscribe(auth => {
+      if(auth) {
+        this.router.navigate(['/']);
+      }
+    })
+  }
+
+  onSubmit() {
+    this.authService.login(this.email, this.password)
+    .then(res => {
+      this.flashMessage.show('You are now logged in.', {cssClass: 'alert-success', timeout: 4000});
+      this.router.navigate(['/']);
+    })
+    .catch(err => {
+      this.flashMessage.show(err.message, { cssClass: 'alert-danger', timeout: 8000 });
+    });
   }
 
 }
